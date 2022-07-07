1601ec5ef49bb9c595239055d3fbd7476498744c
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-07-07 14:56:40 +0900

    특정 문자 뒤집기
    index 0 오류

--- inflearnJavaAlgorithm/ch01-05/src/Main.java
+++ inflearnJavaAlgorithm/ch01-05/src/Main.java
@@ -1,5 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Character.isLetter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static String solution(String str) {
+        String r = "";
+        int[] index = new int[0];
+        int i = 0;
+        char[] ch = str.toCharArray();
+
+        for (int j = ch.length - 1; j >= 0; j--) {
+            if (isLetter(ch[j])) {
+                r += ch[j];
+            }
+            else {
+                index[i] = j;
+                i++;
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(r);
+        for(int j = 0; j < index.length; j++) {
+            sb.insert(index[j], ch[index[j]]);
+        }
+
+        return r;
+    }
+    public static void main(String[] args) throws IOException {
+        String str = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+
+        System.out.println(solution(str));
     }
 }
