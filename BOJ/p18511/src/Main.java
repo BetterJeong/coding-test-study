b551f9d7e3d0b10cd4258232521c4456bc8fda21
Better Jeong <eunjeong42na@gmail.com>
2022-07-08 23:48:09 +0900

    결과 값이 다름
    ㅜㅜ

--- BOJ/p18511/src/Main.java
+++ BOJ/p18511/src/Main.java
@@ -1,22 +1,33 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
     public static int solution(int n ,int[] arr) {
         int r = 0;
+        String sn = String.valueOf(n);
+        char[] ch = sn.toCharArray();
+        int pre = 0;
+        int max = 0;
 
-
+        for(int j = ch.length; j > 0; j--) {
+            max = arr[0];
+            for (int i = 0; i < arr.length; i++) {
+                if (pre*10 + Character.getNumericValue(ch[j-1]) > arr[i] && max < arr[i]) {
+                    max = arr[i];
+                }
+            }
+            r += (max * Math.pow(10, j));
+        }
 
         return r;
     }
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
-        int[] arr = new int[0];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[k];
         for (int i = 0; i < k; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = sc.nextInt();
         }
         System.out.println(solution(n, arr));
     }
