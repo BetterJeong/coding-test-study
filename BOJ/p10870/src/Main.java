38b5037e45dab521fe8a4c1ae9aaa8ce4406c4cd
Better Jeong <eunjeong42na@gmail.com>
2022-07-12 11:02:03 +0900

    10870 피보나치 수 5

---
+++ BOJ/p10870/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static int solution(int n) {
+        if (n == 0) { return 0; }
+        if (n == 1) { return 1; }
+        return solution(n-1) + solution(n-2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(solution(n));
+    }
+}
