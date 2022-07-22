93ab0041552a001052d02225a370ded1502b83fa
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-07-22 17:48:07 +0900

    11729 하노이의 탑

--- BOJ/p11729/src/Main.java
+++ BOJ/p11729/src/Main.java
@@ -1,21 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
-    public static void solution(int n, int from, int tmp, int to) {
+    public static BufferedReader br;
+    public static BufferedWriter bw;
+
+    public static void solution(int n, int from, int tmp, int to) throws IOException {
         if (n == 0) return;
         solution(n-1, from, to, tmp);
-        System.out.println(from + " " + to);
+        bw.write(from + " " + to + "\n");
         solution(n-1, tmp, from, to);
     }
 
     public static void main(String[] args) throws IOException {
         int n = 0;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
         System.out.println((int)Math.pow(2, n) - 1);
         solution(n, 1, 2, 3);
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
