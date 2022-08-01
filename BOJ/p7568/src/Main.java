e823f7d24daaa87feac32ce1ac361fe713d9e2dd
Better Jeong <eunjeong42na@gmail.com>
2022-08-01 16:54:19 +0900

    7568 덩치

--- BOJ/p7568/src/Main.java
+++ BOJ/p7568/src/Main.java
@@ -7,7 +7,14 @@ public class Main {
     public static int[] solution(int[][] arr, int n) {
         int[] r =  new int[n];
 
-
+        for (int i = 0; i < n; i++) {
+            r[i] = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    r[i]++;
+                }
+            }
+        }
 
         return r;
     }
@@ -24,7 +31,7 @@ public class Main {
         }
         int[] r = solution(arr, n);
         for (int i = 0; i < n; i++) {
-            System.out.println(r[i]+" ");
+            System.out.print(r[i]+" ");
         }
     }
 }
