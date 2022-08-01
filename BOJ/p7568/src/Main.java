59b3468dde145b2d554363fb09f45767e4994327
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-08-01 15:58:29 +0900

    7568 풀기 시작

--- BOJ/p7568/src/Main.java
+++ BOJ/p7568/src/Main.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static int[] solution(int[][] arr, int n) {
+        int[] r =  new int[n];
+
+
+
+        return r;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[] r = solution(arr, n);
+        for (int i = 0; i < n; i++) {
+            System.out.println(r[i]+" ");
+        }
     }
 }
