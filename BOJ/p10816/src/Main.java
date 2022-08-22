a81ceee633eb316975dbcf143354c9e04b46a235
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-08-22 14:27:00 +0900

    10816 숫자 카드 2

--- BOJ/p10816/src/Main.java
+++ BOJ/p10816/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -6,9 +7,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-
+            int k = Integer.parseInt(st.nextToken());
+            hashMap.put(k, hashMap.getOrDefault(k, 0) + 1);
         }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int k = Integer.parseInt(st.nextToken());
+            bw.write(hashMap.getOrDefault(k, 0)+" ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
