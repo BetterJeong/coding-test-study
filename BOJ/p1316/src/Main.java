ff3836bba5a21efc485b7cdd05599a4ad2da5cf7
Better Jeong <eunjeong42na@gmail.com>
2022-09-15 00:45:49 +0900

    1316 그룹 단어 체커

--- BOJ/p1316/src/Main.java
+++ BOJ/p1316/src/Main.java
@@ -1,5 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int r = 0;
+
+        for (int i = 0; i < n; i++) {
+            char[] arr = br.readLine().toCharArray();
+            boolean[] alpha = new boolean[26];
+            char pre = arr[0];
+            alpha[arr[0]-'a'] = true;
+            boolean check = true;
+
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j] == pre) {
+                    continue;
+                }
+                else if (alpha[arr[j]-'a'] == false) {
+                    alpha[arr[j]-'a'] = true;
+                }
+                else {
+                    check = false;
+                    break;
+                }
+                pre = arr[j];
+            }
+
+            if (check) {
+                r++;
+            }
+        }
+
+        System.out.println(r);
     }
 }
