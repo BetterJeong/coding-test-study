29b7fe06beb3d0fdece5dfd9558a81e91b750594
Better Jeong <eunjeong42na@gmail.com>
2023-02-06 21:25:49 +0900

    1764 듣보잡

---
+++ BOJ/p1764/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashSet<String> set = new HashSet<>();
+        ArrayList<String> results = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            if (set.contains(str)) {
+                results.add(str);
+            }
+        }
+
+        Collections.sort(results);
+
+        System.out.println(results.size());
+        for (String str : results) {
+            bw.write(str + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
