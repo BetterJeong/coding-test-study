faf5adcc71bce54c8a02bcabce38fb524651843a
Better Jeong <eunjeong42na@gmail.com>
2023-02-25 01:02:49 +0900

    10866 덱

---
+++ BOJ/p10866/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Deque<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+
+            if (str.equals("push_front")) queue.addFirst(Integer.parseInt(st.nextToken()));
+            else if (str.equals("push_back")) queue.addLast(Integer.parseInt(st.nextToken()));
+            else if (str.equals("size")) bw.write(queue.size()+"\n");
+            else if (queue.isEmpty()) {
+                if (str.equals("empty")) bw.write("1\n");
+                else bw.write("-1\n");
+            }
+            else {
+                if (str.equals("pop_front")) bw.write(queue.pollFirst()+"\n");
+                else if (str.equals("pop_back")) bw.write(queue.pollLast()+"\n");
+                else if (str.equals("empty")) bw.write("0\n");
+                else if (str.equals("front")) bw.write(queue.peekFirst()+"\n");
+                else if (str.equals("back")) bw.write(queue.peekLast()+"\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
