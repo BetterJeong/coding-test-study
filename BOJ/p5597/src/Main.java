6534f856009b89cc80ac232397d4c566c922221b
Better Jeong <eunjeong42na@gmail.com>
2023-03-05 14:06:11 +0900

    5597 과제 안 내신 분...?

---
+++ BOJ/p5597/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] b = new boolean[31];
+
+        for (int i = 0; i < 28; i++) b[Integer.parseInt(br.readLine())] = true;
+
+        for (int i = 1; i < 31; i++) if (!b[i]) System.out.println(i);
+    }
+}
