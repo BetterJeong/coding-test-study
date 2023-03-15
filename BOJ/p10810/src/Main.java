19f1c55f0707c3d298ef95986aee9926fb83bf6d
Better Jeong <eunjeong42na@gmail.com>
2023-03-16 00:17:55 +0900

    10810 공 넣기

---
+++ BOJ/p10810/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        for (int t = 0; t < m; t++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            for (; i <= j; i++) {
+                arr[i-1] = k;
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
