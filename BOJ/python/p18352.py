8a26d59e8f577c6fdfac63d3b21c71bab691c84e
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2023-05-25 11:00:06 +0900

    18352 특정 거리의 도시 찾기

--- BOJ/python/p18352.py
+++ BOJ/python/p18352.py
@@ -1,13 +1,41 @@
-import sys
-input = sys.stdin.readline
+import heapq
 INF = int(1e9)
 
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
 N, M, K, X = map(int, input().split())
-
 graph = [[] for i in range(N + 1)]
-visited = [False] * (N + 1)
 distance = [INF] * (N + 1)
+result = []
 
-for _ in range(M):  # 간선 정보 입력
+for _ in range(M):
     a, b = map(int, input().split())
-    graph[a].append((b, c))
+    graph[a].append((b, 1))  # 가중치를 1로 설정
+
+dijkstra(X)
+
+for i in range(1, N + 1):
+    if distance[i] == K:
+        result.append(i)
+
+if len(result) == 0:
+    print(-1)
+else:
+    for i in result:
+        print(i)
