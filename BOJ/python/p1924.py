0fb75cfbf13e3ab5d36e5c3db0735729a214da3a
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2023-08-18 13:18:22 +0900

    Update p1924.py

--- BOJ/python/p1924.py
+++ BOJ/python/p1924.py
@@ -1,24 +1,9 @@
 x, y = map(int, input().split())
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
+w = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
 d = 0
-if x > 1:
-    for i in range(0, x-1):
-        d += days[i]
-d += y
-d %= 7
 
-if d == 0:
-    print("SUN")
-elif d == 1:
-    print("MON")
-elif d == 2:
-    print("TUE")
-elif d == 3:
-    print("WED")
-elif d == 4:
-    print("THU")
-elif d == 5:
-    print("FRI")
-elif d == 6:
-    print("SAT")
+for i in range(0, x - 1):
+    d += days[i]
+
+print(w[(d + y) % 7])
