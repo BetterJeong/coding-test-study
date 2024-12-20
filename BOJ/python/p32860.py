c07167d6dd407925f889166b3a0d8b078e592a85
Better Jeong <eunjeong42na@gmail.com>
2024-12-20 10:16:25 +0900

    Update p32860.py

--- BOJ/python/p32860.py
+++ BOJ/python/p32860.py
@@ -1,6 +1,3 @@
 N, M = map(int, input().split())
 
-if M <= 26:
-    print("SN", str(N) + chr(M + 64))
-else:
-    print("SN", str(N) + chr(96 + (M - 1) // 26) + chr(97 + (M - 1) % 26))
+print("SN", str(N) + chr(M + 64) if M <= 26 else str(N) + chr(96 + (M - 1) // 26) + chr(97 + (M - 1) % 26))
