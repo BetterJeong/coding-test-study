5e76491b3bdfce3f028d0b00c890b05cd9b2f40b
Better Jeong <eunjeong42na@gmail.com>
2025-01-09 09:28:34 +0900

    23809 골뱅이 찍기 - 돌아간 ㅈ

---
+++ BOJ/python/p23809.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+for i in range(N):
+    print("@"*N + "   "*N + "@"*N)
+
+for i in range(N):
+    print("@"*N + "  "*N + "@"*N)
+
+for i in range(N):
+    print("@"*N*3)
+
+for i in range(N):
+    print("@"*N + "  "*N + "@"*N)
+
+for i in range(N):
+    print("@"*N + "   "*N + "@"*N)
